ba7edc691999d7d46fdab4571d0dd8383b392803
waletee <taiwobiodun@gmail.com>
2022-09-21 00:55:17 +0100

    hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(0, 100):
-    if i < 99:
-        print("{:02d},".format(i), end=" ")
+for i in range(100):
+    if i == 99:
+        print(i)
     else:
-        print("{}".format(i))
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")
