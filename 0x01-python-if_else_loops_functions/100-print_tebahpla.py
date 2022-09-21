991b9417cf5747a9e39661677f74c5f5a1e850c2
waletee <taiwobiodun@gmail.com>
2022-09-21 01:08:15 +0100

    python if else loop advanced tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(90, 64, -1):
-    if i % 2 == 0:
-        print("{}".format(chr(i + 32)), end="")
-    else:
-        print("{}".format(chr(i)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    j = 0
-    arr = []
-    for i in str:
-        if j == n:
-            arr.remove(arr[j])
-            j += 1
-        else:
-            arr.append(i)
-    print(arr)
-    return arr
-
-remove_char_at("hello", 3)
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
