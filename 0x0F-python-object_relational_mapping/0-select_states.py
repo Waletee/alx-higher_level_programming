4469c8f5b813d8003f5986c48e5197823dfa1f1b
waletee <taiwobiodun@gmail.com>
2022-12-20 17:06:41 +0100

    hot fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """ 
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                             <mysql password> \
-                             <database name>
+python script that lists all states from the database hbtn_0e_0_usa
 """
 
 import sys
